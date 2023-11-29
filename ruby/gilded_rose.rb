cf85a60928cefdc0e9f2da9e38a737fc40efe483
= <=>
2023-11-29 20:17:16 +0530

    Added Flow for conjour and refactored the code~

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,4 +1,8 @@
 class GildedRose
+  AGED = "Aged Brie" 
+  BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+  SULFUR = "Sulfuras, Hand of Ragnaros"
+  CONJOUR = "Conjured Mana Cake"
 
   def initialize(items)
     @items = items
@@ -6,50 +10,67 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      if item.name != AGED and item.name != BACKSTAGE
+        #decreasing quality of items, other than following
+        decrease_quality_for_items(item)
       else
+        #checking of exceptions
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+          #checks for name
+          if item.name == BACKSTAGE
+            item = increment_quality_for_backstage(item)
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      #decrease the days
+      update_item_sell_in(item)
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        sell_in_lesser_than_zero(item)
       end
     end
+  end
+
+  private
+
+  def increment_quality_for_backstage(item)
+    item.quality = item.quality + 1 if item.sell_in < 11 and item.quality < 50
+    item.quality = item.quality + 1 if item.sell_in < 6 and item.quality < 50
+    item
+  end
+
+  def decrease_quality_for_items(item)
+    if item.quality > 0
+      item.quality = item.quality - 1 if item.name != SULFUR
+      #double decrease in quality
+      if item.name == CONJOUR
+        item.quality = item.quality - 1 if  item.quality >= 1
+      end 
+    end
+    item
+  end
+
+  def update_item_sell_in(item)
+    if item.name == SULFUR
+      item
+    else
+      item.sell_in -= 1
+    end
+    item
+  end
+
+  def sell_in_lesser_than_zero(item)
+    if item.name != AGED
+      if item.name != BACKSTAGE
+        #quantity decreasing
+        item = decrease_quality_for_items(item)
+      else
+        item.quality = item.quality - item.quality
+      end
+    else
+      item.quality = item.quality + 1 if item.quality < 50
+    end
+    item
   end
 end
 
