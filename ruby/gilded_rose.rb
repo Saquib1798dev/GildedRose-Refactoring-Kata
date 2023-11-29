dcde3dfd5988af333aad8b3fbaa42c02e6354db9
= <=>
2023-11-29 20:29:36 +0530

    Multiple Refactoring of Code into various Private Methods

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,41 +10,37 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != AGED and item.name != BACKSTAGE
+      if [AGED, BACKSTAGE].include?(item.name)
+        if item.quality < 50
+          item.quality += 1
+          #checks for name
+          item = increment_quality_for_backstage(item) if item.name == BACKSTAGE
+        end
+        #checking of exceptions
+      else
         #decreasing quality of items, other than following
         decrease_quality_for_items(item)
-      else
-        #checking of exceptions
-        if item.quality < 50
-          item.quality = item.quality + 1
-          #checks for name
-          if item.name == BACKSTAGE
-            item = increment_quality_for_backstage(item)
-          end
-        end
       end
       #decrease the days
       update_item_sell_in(item)
-      if item.sell_in < 0
-        sell_in_lesser_than_zero(item)
-      end
+      sell_in_lesser_than_zero(item) if item.sell_in < 0
     end
   end
 
   private
 
   def increment_quality_for_backstage(item)
-    item.quality = item.quality + 1 if item.sell_in < 11 and item.quality < 50
-    item.quality = item.quality + 1 if item.sell_in < 6 and item.quality < 50
+    item.quality += 1 if item.sell_in < 11 and item.quality < 50
+    item.quality += 1 if item.sell_in < 6 and item.quality < 50
     item
   end
 
   def decrease_quality_for_items(item)
     if item.quality > 0
-      item.quality = item.quality - 1 if item.name != SULFUR
+      item.quality -= 1 if item.name != SULFUR
       #double decrease in quality
       if item.name == CONJOUR
-        item.quality = item.quality - 1 if  item.quality >= 1
+        item.quality -= 1 if  item.quality >= 1
       end 
     end
     item
@@ -68,7 +64,7 @@ class GildedRose
         item.quality = item.quality - item.quality
       end
     else
-      item.quality = item.quality + 1 if item.quality < 50
+      item.quality += 1 if item.quality < 50
     end
     item
   end
